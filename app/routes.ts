ec57e780ce204edffb32c8e530421c1d87b92d53
alejanf2885 <alumno.648000@ies-azarquiel.es>
2025-03-10 08:42:36 +0100

    diseño estructurado

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,10 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"), 
-    route("characters", "routes/ListadoCharacters.tsx"), 
-    route("characters/:id", "routes/DetailCharacter.tsx"), 
-    route("guessCharacter", "routes/GameCharacter.tsx"),
-
-  
+  layout("./routes/Layout.tsx", [
+    index("./routes/home.tsx"),
+    route("characters", "./routes/ListadoCharacters.tsx"),
+    route("characters/:id", "./routes/DetailCharacter.tsx"),
+    route("guessCharacter", "./routes/GameCharacter.tsx"),
+  ]),
 ] satisfies RouteConfig;
